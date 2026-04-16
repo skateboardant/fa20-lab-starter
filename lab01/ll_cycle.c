8f7cc2df309199a08f56455c3a481597e93234b2
skateboardant <ley27510@gmail.com>
2026-04-16 11:53:59 +0800

    valgrind use & pointer first editted

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,11 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+	struct node *tortoise = head, *hare = head;
+	while(hare && hare->next){
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if(hare == tortoise) return 1;
+	}
     return 0;
 }
