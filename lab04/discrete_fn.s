bed4ced0e227f6f6beb6555a1500e40c6a1eb2ef
skateboardurant <Y3508322334@hotmail.com>
2026-05-08 14:45:59 +0800

    implementation for discete-value fuction f

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -77,7 +77,11 @@ main:
 # Think: why might having a1 be useful?
 f:
     # YOUR CODE GOES HERE!
-
+    addi t0, a0, 3
+    slli t1, t0, 2
+    add t2, a1, t1
+    lw t3, 0(t2)
+    mv a0, t3 
     jr ra               # Always remember to jr ra after your function!
 
 print_int:
